0af5412ba0806cc31f33158415b872b6b1609694
MehadiFalamazi <falamarzijahromi.mm@gmail.com>
2023-03-06 20:38:22 +0330

    Modified Madiator Extension To Be Async

--- src/MediatR.Latching/MediatorExtensions.cs
+++ src/MediatR.Latching/MediatorExtensions.cs
@@ -1,16 +1,17 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace MediatR.Latching
 {
     public static class MediatorExtensions
     {
-        public static void SendRequest<TRequest>(this IMediator mediator, TRequest request)
+        public static Task<Unit> SendRequest<TRequest>(this IMediator mediator, TRequest request)
         {
             var simpleRequestWrapperType = typeof(SimpleRequestWrapper<>).MakeGenericType(typeof(TRequest));
 
             var simpleRequest = Activator.CreateInstance(simpleRequestWrapperType, request) as IRequest<Unit>;
 
-            mediator.Send(simpleRequest);
+            return mediator.Send(simpleRequest);
         }
     }
 }
