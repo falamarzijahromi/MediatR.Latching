6c9dc2f5a0ab9f693fd80a2ebf141800da77b622
MehadiFalamazi <falamarzijahromi.mm@gmail.com>
2023-03-09 02:07:15 +0330

    Added ServiceCollection Test

---
+++ src/MediatR.Latching.Tests/ServiceCollectionExtensionTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MediatR.Latching.Tests
+{
+    public class ServiceCollectionExtensionTests
+    {
+        [Fact]
+        public void Related_Service_Descriptors_Must_Be_Added()
+        {
+            var services = new ServiceCollection();
+
+
+            var thisAssembly = this.GetType().Assembly;
+
+            services
+                .LatchRequestHandlers<ISimpleRequestHandler<ISimpleRequest>, ISimpleRequest>(
+                lookInto: this.GetType().Assembly,
+                by: (handler, request) => handler.ShouldHandle(request));
+
+
+            Assert.NotEmpty(services);
+            Assert.Equal(3, services.Count);
+        }
+    }
+}
